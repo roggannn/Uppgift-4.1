a81c6b7783f52614b2540b64a98bd92b20d7d02e
Pingvu <nhlpool27@gmail.com>
2023-11-30 12:26:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int tal = 10;
+            while (tal != 30)
+            {
+                tal++;
+                Console.WriteLine(tal);
+            }
+            tal = 201;
+            while (tal != 180)
+            {
+                tal--;
+                Console.WriteLine(tal);
+            }
+            tal = 950;
+            while (tal != 1450)
+            {
+                tal += 50;
+                Console.WriteLine(tal);
+            }
+            Console.ReadKey();
+        }
+    }
+}
